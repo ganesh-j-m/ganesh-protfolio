253d791665f5abe4c2cbe45f7eeb1a279aa503ee
ganesh-j-m <ganeshmore1552@gmail.com>
2025-10-16 02:37:34 -0700

    Add new page Skill

---
+++ Skill.js
@@ -0,0 +1,10 @@
+
+function humburg() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel() {
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
